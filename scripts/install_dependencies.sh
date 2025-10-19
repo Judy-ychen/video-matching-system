e62f4b9b9e136dad5322a737889b057474853c59
Judy-ychen <ychen19@conncoll.edu>
2025-10-19 10:47:14 -0400

    modified appspec.yml, added requirments.txt, modified all scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,39 @@
-echo "start install dependencies"
+#!/usr/bin/env bash
+set -e
+
+APP_DIR="/opt/video-matching-system"
+VENV_DIR="${APP_DIR}/venv"
+SERVICE_FILE="/etc/systemd/system/fastapi-app.service"
+
+# install dependencies
+sudo yum update -y
+sudo yum install -y python3
+python3 -m venv /opt/video-matching-system/venv
+
+# 创建虚拟环境
+python3 -m venv ${VENV_DIR}
+source ${VENV_DIR}/bin/activate
+pip install --upgrade pip
+pip install -r ${APP_DIR}/requirements.txt
+deactivate
+
+# 创建 systemd 服务文件
+cat > ${SERVICE_FILE} <<EOF
+[Unit]
+Description=FastAPI Hello World Service
+After=network.target
+
+[Service]
+User=ec2-user
+WorkingDirectory=${APP_DIR}
+Environment="PATH=${VENV_DIR}/bin"
+ExecStart=${VENV_DIR}/bin/uvicorn src.main:app --host 0.0.0.0 --port 8000
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 启用服务
+sudo systemctl daemon-reload
+sudo systemctl enable fastapi-app
